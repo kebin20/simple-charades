760165cd8c6ff7d27296369e8d269f564f55efe2
kebin20 <ktanzyl@hotmail.com>
2022-09-20 16:52:58 +0900

    Added files coded from codepen and img files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # simple-charades
 A simple app that helps with the english game for charades
+
+The basic functions are that firstly, a random image will appear after clicking a button. These images are provided by providing them within a source folder. 
+
+Buttons and counters are displayed below the image to keep track of scores and number of skips. 
+
+This project was mainly a practice for DOM manipulating and basic Vanilla Javacsript. 
+
+Future function to add:
+
+- The function for users to upload their own images in the app so that it can render other people's images.
---
+++ app.js
@@ -0,0 +1,102 @@
+// Global variables
+let correctAmt = 0;
+let skipAmt = 0;
+let MaxSkip = 0;
+
+// HTML Components
+
+// Buttons
+const nextBtn = document.querySelector("#next-btn");
+const resetBtn = document.querySelector("#reset-btn");
+const correctBtn = document.querySelector("#correct-btn");
+const skipBtn = document.querySelector("#skip-btn");
+const maxSkips = document.querySelector("#max-skips");
+
+// Displays
+const correctCtr = document.querySelector(".correct-ctr");
+const skipCtr = document.querySelector(".skip-ctr");
+const image = document.querySelector("img");
+
+// To randomise images
+let imgArray = [
+  "img-01.png",
+  "img-02.png",
+  "img-03.png",
+  "img-04.png",
+  "img-05.png",
+  "img-06.png",
+  "img-07.png",
+  "img-08.png",
+  "img-09.png",
+  "img-10.png",
+  "img-11.png",
+  "img-12.png",
+  "img-13.png",
+  "img-14.png",
+  "img-15.png",
+  "img-16.png",
+  "img-17.png",
+  "img-18.png",
+  "img-19.png",
+  "img-20.png",
+  "img-21.png",
+  "img-22.png",
+  "img-23.png",
+  "img-24.png",
+  "img-25.png",
+  "img-26.png",
+  "img-27.png",
+  "img-28.png",
+  "img-29.png",
+  "img-30.png",
+  "img-31.png",
+  "img-32.png",
+];
+
+// let basePath = "img/";
+
+// function imgRandom() {
+//     for(i = 0; i < 10; i++)
+//   const rand = imgArray[Math.floor(Math.random() * imgArray.length)];
+//   let image = new Image();
+//   image.src = basePath + rand;
+//   displayImg.appendChild(image);
+// }
+
+window.onload = () => imgRandom(imgArray);
+
+function imgRandom(array) {
+  let randomNum = Math.floor(Math.random() * array.length);
+  image.setAttribute("src", `img/${imgArray[randomNum]}`);
+  console.log(randomNum);
+}
+
+function reset() {
+  correctAmt = 0;
+  skipAmt = 0;
+  correctCtr.textContent = correctAmt;
+  skipCtr.textContent = skipAmt;
+  skipBtn.disabled = false;
+}
+
+//Button and counter functions
+nextBtn.addEventListener("click", () => imgRandom(imgArray));
+
+resetBtn.addEventListener("click", reset);
+
+correctBtn.addEventListener("click", () => {
+  correctAmt += 1;
+  correctCtr.textContent = correctAmt;
+});
+
+skipBtn.addEventListener("click", () => {
+  skipAmt += 1;
+  skipCtr.textContent = skipAmt;
+  if (skipAmt === MaxSkip) {
+    skipBtn.disabled = true;
+  }
+});
+
+maxSkips.addEventListener("change", (e) => {
+  MaxSkip = parseInt(e.target.value);
+});
