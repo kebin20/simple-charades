0c15ca2e6d6a5fcced6b3787b38391e66e8701d7
kebin20 <ktanzyl@hotmail.com>
2022-09-25 07:33:20 +0900

    Modified code, updated readme and added notes

--- README.md
+++ README.md
@@ -13,6 +13,14 @@ This project was mainly a practice for DOM manipulating and basic Vanilla Javacs
 
 I feel like this solution is the most time saving as you do not need to hard-code the image numbers in the array anymore. If you need to increase or decrease the number of images, simply change the number of arrays needed in Array() and change the value in the random function.  In addition, this also helps to reduce the lines of code making my code DRYer. 
 
+25/09/22
+
+- Had a pair programming session with a friend. He helped me to change the code so that the same image won't appear twice when clicking through the next button. We used the .sort() function to pass in a function that produces a random number in order to randomise the numbers in the array. 
+
+- One thing to note is to NOT use .map() with DOM manipulation as the use for .map() as it is not good practice and shouldn't be used for DOM manipulation (such as using .innerHTML)
+
+
+
 
 Future function to add:
 
--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let correctAmt = 0;
 let skipAmt = 0;
 let MaxSkip = 0;
+let images = initializeImages();
+let index = 0;
 
 // HTML Components
 
@@ -17,78 +19,34 @@ const correctCtr = document.querySelector(".correct-ctr");
 const skipCtr = document.querySelector(".skip-ctr");
 const image = document.querySelector("img");
 
-// 1st solution
-// let basePath = "img/";
+window.onload = () =>
+  (document.getElementById("img-display").innerHTML = images[0]);
 
-// function imgRandom() {
-//     for(i = 0; i < 10; i++)
-//   const rand = imgArray[Math.floor(Math.random() * imgArray.length)];
-//   let image = new Image();
-//   image.src = basePath + rand;
-//   displayImg.appendChild(image);
-// }
-
-// 2nd solution
-// To randomise images
-// let imgArray = [
-//   "img-01.png",
-//   "img-02.png",
-//   "img-03.png",
-//   "img-04.png",
-//   "img-05.png",
-//   "img-06.png",
-//   "img-07.png",
-//   "img-08.png",
-//   "img-09.png",
-//   "img-10.png",
-//   "img-11.png",
-//   "img-12.png",
-//   "img-13.png",
-//   "img-14.png",
-//   "img-15.png",
-//   "img-16.png",
-//   "img-17.png",
-//   "img-18.png",
-//   "img-19.png",
-//   "img-20.png",
-//   "img-21.png",
-//   "img-22.png",
-//   "img-23.png",
-//   "img-24.png",
-//   "img-25.png",
-//   "img-26.png",
-//   "img-27.png",
-//   "img-28.png",
-//   "img-29.png",
-//   "img-30.png",
-//   "img-31.png",
-//   "img-32.png",
-// ];
-
-// window.onload = () => imgRandom(imgArray);
-
-// function imgRandom(array) {
-//   let randomNum = Math.floor(Math.random() * array.length);
-//   image.setAttribute("src", `img/${imgArray[randomNum]}`);
-//   console.log(randomNum);
-// }
-// nextBtn.addEventListener("click", () => imgRandom(imgArray));
-
-// 3rd solution
-window.onload = () => getImages();
-
-function getRandomImgArray() {
-  return new Array(32).fill(0).map(() => {
-    return Math.floor(Math.random() * 32) + 1;
-  });
+function initializeImages() {
+  return (
+    new Array(32)
+      .fill(0)
+      //The value in this .map()  is at 0, but the index (the array number) is always different, therefore we target this index part and randomise this part.
+      .map((value, index) => {
+        return index + 1;
+      })
+      .map((number) => {
+        return `<img src="img/img-${number}.png" class="display-img">`;
+      })
+      .sort(randomSort)
+  );
 }
 
-function getImages() {
-  return getRandomImgArray().map((number) => {
-    return (document.getElementById(
-      "img-display"
-    ).innerHTML = `<img src="img/img-${number}.png" class="display-img">`);
-  });
+function randomSort(a, b) {
+  return Math.random() < 0.5 ? -1 : 1;
+}
+
+function nextImage() {
+  index++;
+  if (index >= 32) {
+    index = 0;
+  }
+  document.getElementById("img-display").innerHTML = images[index];
 }
 
 function reset() {
@@ -100,7 +58,7 @@ function reset() {
 }
 
 //Button and counter functions
-nextBtn.addEventListener("click", () => getImages());
+nextBtn.addEventListener("click", () => nextImage());
 
 resetBtn.addEventListener("click", reset);
 
